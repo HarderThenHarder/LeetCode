b1d4066d64c2ae6d3801cd8ac93fa727ab61a828
Pky <1414463123@qq.com>
2018-08-24 17:19:24 +0800

    Add some method

--- ArrayAlgorithm.py
+++ ArrayAlgorithm.py
@@ -140,3 +140,41 @@ class ArrayAlgorithm:
                 matrix[index][i], matrix[index][j] = matrix[index][j], matrix[index][i]
             i += 1
             j -= 1
+
+    """Given two arrays, write a function to compute their intersection.Ex: [4,4,2,2] & [2,4] -> [2,4]"""
+    @staticmethod
+    def intersect(nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        repeat = []
+        nums1.sort()
+        nums2.sort()
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                repeat.append(nums1[i])
+                i += 1
+                j += 1
+
+        return repeat
+
+    """Best Time to Buy and Sell Stock II"""
+    @staticmethod
+    def max_profit(prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += (prices[i] - prices[i-1])
+        return profit
