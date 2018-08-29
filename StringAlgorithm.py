45e838f00f2b31f251e84a9f65b18dac6e57c00c
Pky <1414463123@qq.com>
2018-08-29 15:44:24 +0800

    Add StringAlgorithm class

--- StringAlgorithm.py
+++ StringAlgorithm.py
@@ -2,7 +2,7 @@ class StringAlgorithm:
 
     """reverse str, don't use method->s[::-1]"""
     @staticmethod
-    def reverseString(s):
+    def reverse_string(s):
         """
         :type s: str
         :rtype: str
@@ -11,4 +11,28 @@ class StringAlgorithm:
         max_index = len(s) - 1
         for index, value in enumerate(s):
             res += s[max_index - index]
+        return res
+
+    """reverse int: 123 -> 321; -123 -> -321"""
+    @staticmethod
+    def reverse_num(x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        res = 0
+        while x != 0:
+            print(res)
+            if abs(res) > (2 ** 31 - 1) / 10:
+                return 0
+            if x > 0:
+                res = res * 10 + x % 10
+                x //= 10
+            elif x < 0:
+                if x % 10 != 0:
+                    res = res * 10 - (10 - x % 10)
+                    x = x // 10 + 1
+                else:
+                    res = res * 10
+                    x = x // 10
         return res
